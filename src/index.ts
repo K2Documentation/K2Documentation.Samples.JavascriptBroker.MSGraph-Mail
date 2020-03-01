d9442b53ebcfc419637eb2be121bfb51f24be7b0
michta42 <58866449+michta42@users.noreply.github.com>
2020-02-29 19:19:18 -0700

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -9,40 +9,40 @@ metadata = {
 
 ondescribe = function() {
     postSchema({ objects: {
-                "com.k2.sample.msgraph.mail.message": {
+                "message": {
                     displayName: "Message",
                     description: "An Outlook message",
                     properties: {
-                        "com.k2.sample.msgraph.mail.message.id": {
+                        "id": {
                             displayName: "ID",
                             type: "string" 
                         },
-                        "com.k2.sample.msgraph.mail.message.subject": {
+                        "subject": {
                             displayName: "Subject",
                             type: "string"
                         },
-                        "com.k2.sample.msgraph.mail.message.from": {
+                        "from": {
                             displayName: "From",
                             type: "string"
                         },
-                        "com.k2.sample.msgraph.mail.message.body": {
+                        "body": {
                             displayName: "Body",
                             type: "string"
                         }
                     },
                     methods: {
-                        "com.k2.sample.msgraph.mail.message.get": {
+                        "get": {
                             displayName: "Get Message",
                             type: "read",
-                            inputs: ["com.k2.sample.msgraph.accessToken","com.k2.sample.msgraph.mail.message.userPrincipalName","com.k2.sample.msgraph.mail.message.id"],
-                            requiredInputs: [ "com.k2.sample.msgraph.mail.message.id" , "com.k2.sample.msgraph.mail.message.userPrincipalName"],
-                            outputs: [ "com.k2.sample.msgraph.mail.message.id", "com.k2.sample.msgraph.mail.message.subject", "com.k2.sample.msgraph.mail.message.from", "com.k2.sample.msgraph.mail.message.body" ]
+                            inputs: ["accessToken","userPrincipalName","id"],
+                            requiredInputs: [ "id" , "userPrincipalName"],
+                            outputs: [ "id", "subject", "from", "body" ]
                         },
-                        "com.k2.sample.msgraph.mail.message.list": {
+                        "list": {
                             displayName: "Get List",
                             type: "list",
-                            inputs: ["com.k2.sample.msgraph.accessToken","com.k2.sample.msgraph.mail.message.userPrincipalName"],
-                            outputs: [ "com.k2.sample.msgraph.mail.message.id", "com.k2.sample.msgraph.mail.message.subject", "com.k2.sample.msgraph.mail.message.from", "com.k2.sample.msgraph.mail.message.body" ]
+                            inputs: ["accessToken","userPrincipalName"],
+                            outputs: [ "id", "subject", "from", "body" ]
                         }
                     }
                 }
@@ -53,38 +53,38 @@ ondescribe = function() {
 onexecute = function(objectName, methodName, parameters, properties) {
     switch (objectName)
     {
-        case "com.k2.sample.msgraph.mail.message": onexecuteMessage(methodName, parameters, properties); break;
+        case "message": onexecuteMessage(methodName, parameters, properties); break;
         default: throw new Error("The object " + objectName + " is not supported.");
     }
 }
 
-function onexecuteMessage(methodName: string, parameters: SingleRecord, properties: SingleRecord) {
+ function onexecuteMessage(methodName: string, parameters: SingleRecord, properties: SingleRecord) {
     switch (methodName)
     {
-        case "com.k2.sample.msgraph.mail.message.get": onexecuteMessageGet(parameters, properties); break;
-        case "com.k2.sample.msgraph.mail.message.list": onexecuteMessageList(parameters, properties); break;
+        case "get": onexecuteMessageGet(parameters, properties); break;
+        case "list": onexecuteMessageList(parameters, properties); break;
         default: throw new Error("The method " + methodName + " is not supported.");
     }
 }
 
 function onexecuteMessageGet(parameters: SingleRecord, properties: SingleRecord) {
     var xhr = new XMLHttpRequest();
-    
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState !== 4) return;
-        if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
 
-        //console.log(xhr.responseText);
-        var obj = JSON.parse(xhr.responseText);
-        postResult({
-            "com.k2.sample.msgraph.mail.message.id": obj.id,
-            "com.k2.sample.msgraph.mail.message.subject": obj.subject,
-            "com.k2.sample.msgraph.mail.message.from": obj.from.emailAddress.address,
-            "com.k2.sample.msgraph.mail.message.body": obj.body.content
-        }); 
+    xhr.onreadystatechange = function() {
+            if (xhr.readyState !== 4) return;
+            if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
+
+            //console.log(xhr.responseText);
+            var obj = JSON.parse(xhr.responseText);
+            postResult({
+                "id": obj.id,
+                "subject": obj.subject,
+                "from": obj.from.emailAddress.address,
+                "body": obj.body.content
+            });            
     };
 
-    var url = "https://graph.microsoft.com/v1.0/users/" + parameters["com.k2.sample.msgraph.mail.message.userPrincipalName"] + "/mailfolders%28%27Inbox%27%29/messages/" + parameters["com.k2.sample.msgraph.mail.message.id"];
+    var url = "https://graph.microsoft.com/v1.0/users/" + encodeURIComponent(parameters["userPrincipalName"]) + "/mailfolders%28%27Inbox%27%29/messages/" + encodeURIComponent(parameters["id"]);
     //console.log(url);
     xhr.open("GET", url);
 
@@ -92,34 +92,35 @@ function onexecuteMessageGet(parameters: SingleRecord, properties: SingleRecord)
     //xhr.withCredentials = true;
 
     // Use Access Token
-    xhr.setRequestHeader("Authorization", "Bearer " + parameters["com.k2.sample.msgraph.accessToken"]);
-    xhr.send();
+    xhr.setRequestHeader("Authorization", "Bearer " + encodeURIComponent(parameters["accessToken"]));
+    xhr.send();   
 }
 
 function onexecuteMessageList(parameters: SingleRecord, properties: SingleRecord) {
     var xhr = new XMLHttpRequest();
-    
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState !== 4) return;
-        if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
 
-        //console.log(xhr.responseText);
-        var obj = JSON.parse(xhr.responseText);
-        for (var key in obj) {
-            postResult({
-            "com.k2.sample.msgraph.mail.message.id": obj[key].id,
-            "com.k2.sample.msgraph.mail.message.subject": obj[key].subject,
-            "com.k2.sample.msgraph.mail.message.from": obj[key].from.emailAddress.address,
-            "com.k2.sample.msgraph.mail.message.body": obj[key].body.content
-        }})); 
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+    xhr.onreadystatechange = function() {
+            if (xhr.readyState !== 4) return;
+            if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
+
+            //console.log(xhr.responseText);
+            var obj = JSON.parse(xhr.responseText);
+            postResult(obj.map(x => {
+                return {
+                    "id": x.id,
+                    "subject": x.subject,
+                    "from": x.from.emailAddress.address,
+                    "body": x.body.content
+                }
+            }));
     };
 
-    var url = "https://graph.microsoft.com/v1.0/users/" + parameters["com.k2.sample.msgraph.mail.message.userPrincipalName"] + "/mailfolders%28%27Inbox%27%29/messages";
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+    var url = "https://graph.microsoft.com/v1.0/users/" + encodeURIComponent(parameters["userPrincipalName"]) + "/mailfolders%28%27Inbox%27%29/messages";
     //console.log(url);
     xhr.open("GET", url);
 
     // Use Access Tokens
-    xhr.setRequestHeader("Authorization", "Bearer " + parameters["com.k2.sample.msgraph.accessToken"]);
-    xhr.send();
+    xhr.setRequestHeader("Authorization", "Bearer " + encodeURIComponent(parameters["accessToken"]));
+    xhr.send(); 
 }
