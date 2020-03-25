47c97bf87699921ba062b7cd96d89759a2109fec
reji <60493584+rejik2@users.noreply.github.com>
2020-03-25 15:38:58 -0700

    Reji/update samples (#2)
    * updating npm packages and adding .gitignore
    
    * adding promise and async support
    
    * updating the test.ts
    
    * adding awaits
    
    * cleanup

--- src/index.ts
+++ src/index.ts
@@ -7,120 +7,128 @@ metadata = {
     // https://docs.microsoft.com/en-us/graph/outlook-share-messages-folders
 };
 
-ondescribe = function() {
-    postSchema({ objects: {
-                "message": {
-                    displayName: "Message",
-                    description: "An Outlook message",
-                    properties: {
-                        "id": {
-                            displayName: "ID",
-                            type: "string" 
-                        },
-                        "subject": {
-                            displayName: "Subject",
-                            type: "string"
-                        },
-                        "from": {
-                            displayName: "From",
-                            type: "string"
-                        },
-                        "body": {
-                            displayName: "Body",
-                            type: "string"
-                        }
+ondescribe = async function (): Promise<void> {
+    postSchema({
+        objects: {
+            "message": {
+                displayName: "Message",
+                description: "An Outlook message",
+                properties: {
+                    "id": {
+                        displayName: "ID",
+                        type: "string"
                     },
-                    methods: {
-                        "get": {
-                            displayName: "Get Message",
-                            type: "read",
-                            inputs: ["accessToken","userPrincipalName","id"],
-                            requiredInputs: [ "id" , "userPrincipalName"],
-                            outputs: [ "id", "subject", "from", "body" ]
-                        },
-                        "list": {
-                            displayName: "Get List",
-                            type: "list",
-                            inputs: ["accessToken","userPrincipalName"],
-                            outputs: [ "id", "subject", "from", "body" ]
-                        }
+                    "subject": {
+                        displayName: "Subject",
+                        type: "string"
+                    },
+                    "from": {
+                        displayName: "From",
+                        type: "string"
+                    },
+                    "body": {
+                        displayName: "Body",
+                        type: "string"
+                    }
+                },
+                methods: {
+                    "get": {
+                        displayName: "Get Message",
+                        type: "read",
+                        inputs: ["accessToken", "userPrincipalName", "id"],
+                        requiredInputs: ["id", "userPrincipalName"],
+                        outputs: ["id", "subject", "from", "body"]
+                    },
+                    "list": {
+                        displayName: "Get List",
+                        type: "list",
+                        inputs: ["accessToken", "userPrincipalName"],
+                        outputs: ["id", "subject", "from", "body"]
                     }
                 }
             }
         }
-)};
+    })
+};
 
-onexecute = function(objectName, methodName, parameters, properties) {
-    switch (objectName)
-    {
-        case "message": onexecuteMessage(methodName, parameters, properties); break;
+onexecute = async function (objectName, methodName, parameters, properties): Promise<void> {
+    switch (objectName) {
+        case "message": await onexecuteMessage(methodName, parameters, properties); break;
         default: throw new Error("The object " + objectName + " is not supported.");
     }
 }
 
- function onexecuteMessage(methodName: string, parameters: SingleRecord, properties: SingleRecord) {
-    switch (methodName)
-    {
-        case "get": onexecuteMessageGet(parameters, properties); break;
-        case "list": onexecuteMessageList(parameters, properties); break;
+async function onexecuteMessage(methodName: string, parameters: SingleRecord, properties: SingleRecord): Promise<void> {
+    switch (methodName) {
+        case "get": await onexecuteMessageGet(parameters, properties); break;
+        case "list": await onexecuteMessageList(parameters, properties); break;
         default: throw new Error("The method " + methodName + " is not supported.");
     }
 }
 
-function onexecuteMessageGet(parameters: SingleRecord, properties: SingleRecord) {
-    var xhr = new XMLHttpRequest();
+function onexecuteMessageGet(parameters: SingleRecord, properties: SingleRecord): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
-            if (xhr.readyState !== 4) return;
-            if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
+        xhr.onreadystatechange = function () {
+            try {
+                if (xhr.readyState !== 4) return;
+                if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
+                var obj = JSON.parse(xhr.responseText);
+                postResult({
+                    "id": obj.id,
+                    "subject": obj.subject,
+                    "from": obj.from.emailAddress.address,
+                    "body": obj.body.content
+                });
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
+        };
 
-            //console.log(xhr.responseText);
-            var obj = JSON.parse(xhr.responseText);
-            postResult({
-                "id": obj.id,
-                "subject": obj.subject,
-                "from": obj.from.emailAddress.address,
-                "body": obj.body.content
-            });            
-    };
+        var url = "https://graph.microsoft.com/v1.0/users/" + encodeURIComponent(parameters["userPrincipalName"]) + "/mailfolders%28%27Inbox%27%29/messages/" + encodeURIComponent(parameters["id"]);
+        xhr.open("GET", url);
 
-    var url = "https://graph.microsoft.com/v1.0/users/" + encodeURIComponent(parameters["userPrincipalName"]) + "/mailfolders%28%27Inbox%27%29/messages/" + encodeURIComponent(parameters["id"]);
-    //console.log(url);
-    xhr.open("GET", url);
+        // Use Service Instance OAuth configuration
+        //xhr.withCredentials = true;
 
-    // Use Service Instance OAuth configuration
-    //xhr.withCredentials = true;
-
-    // Use Access Token
-    xhr.setRequestHeader("Authorization", "Bearer " + encodeURIComponent(parameters["accessToken"]));
-    xhr.send();   
+        // Use Access Token
+        xhr.setRequestHeader("Authorization", "Bearer " + encodeURIComponent(parameters["accessToken"]));
+        xhr.send();
+    });
 }
 
-function onexecuteMessageList(parameters: SingleRecord, properties: SingleRecord) {
-    var xhr = new XMLHttpRequest();
+function onexecuteMessageList(parameters: SingleRecord, properties: SingleRecord): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
-            if (xhr.readyState !== 4) return;
-            if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
+        xhr.onreadystatechange = function () {
+            try {
+                if (xhr.readyState !== 4) return;
+                if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
 
-            //console.log(xhr.responseText);
-            var obj = JSON.parse(xhr.responseText);
-            postResult(obj.map(x => {
-                return {
-                    "id": x.id,
-                    "subject": x.subject,
-                    "from": x.from.emailAddress.address,
-                    "body": x.body.content
-                }
-            }));
-    };
+                var obj = JSON.parse(xhr.responseText);
+                postResult(obj.map(x => {
+                    return {
+                        "id": x.id,
+                        "subject": x.subject,
+                        "from": x.from.emailAddress.address,
+                        "body": x.body.content
+                    }
+                }));
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
+        };
 
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-    var url = "https://graph.microsoft.com/v1.0/users/" + encodeURIComponent(parameters["userPrincipalName"]) + "/mailfolders%28%27Inbox%27%29/messages";
-    //console.log(url);
-    xhr.open("GET", url);
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+        var url = "https://graph.microsoft.com/v1.0/users/" + encodeURIComponent(parameters["userPrincipalName"]) + "/mailfolders%28%27Inbox%27%29/messages";
+        xhr.open("GET", url);
 
-    // Use Access Tokens
-    xhr.setRequestHeader("Authorization", "Bearer " + encodeURIComponent(parameters["accessToken"]));
-    xhr.send(); 
+        // Use Access Tokens
+        xhr.setRequestHeader("Authorization", "Bearer " + encodeURIComponent(parameters["accessToken"]));
+        xhr.send();
+    });
 }
